d0675b388bc66e0788c65417793a9fd65564bb1d
DevProCanidia <canniggya@gmail.com>
2023-02-07 21:39:07 -0300

    first commit

---
+++ carrossel.js
@@ -0,0 +1,20 @@
+//carrossel padrão do plugin slick
+
+$('.container-slider').slick({
+    dots: true,
+    arrows:false,
+    speed:1000,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    pauseOnHover:false,
+    responsive: [
+                {
+                  breakpoint: 768,
+                  settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                  }
+                }],
+    });
